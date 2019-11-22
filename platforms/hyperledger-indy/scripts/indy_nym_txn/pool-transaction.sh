180d7e2631b946934ffb1946112618a60e1adaab
Lal <himanshu.a.lal@accenture.com>
2019-11-22 13:19:02 +0530

    Checks Added
    Signed-off-by: Lal <himanshu.a.lal@accenture.com>

--- platforms/hyperledger-indy/scripts/indy_nym_txn/pool-transaction.sh
+++ platforms/hyperledger-indy/scripts/indy_nym_txn/pool-transaction.sh
@@ -9,6 +9,20 @@ sudo apt-get install -y libindy
 
 sudo apt-get install -y indy-cli
 
-indy-cli generate_txn.txt
+indy-cli generate_txn.txt >> result.txt
+
+if grep -q 'Following NYM has been received' 'result.txt'
+then
+	echo "Transaction successful, NYM has been received"
+else
+	echo "Transaction Failed"
+	if grep -q 'Batch execution failed' 'result.txt'
+		no=$(grep -oP "#\w" result.txt | tail -c 2)
+		echo "Script Failed at line: $no"
+		echo $(sed "${no}q;d" generate_txn.txt)
+	fi
+fi
+		
+	
 
 
