49b0a2fa415bac8bcb0041152aae253f7bc88b01
Lal <himanshu.a.lal@accenture.com>
2019-11-26 16:15:30 +0530

    Vault Setup Script Added and Paths corrected
    Signed-off-by: Lal <himanshu.a.lal@accenture.com>

--- platforms/hyperledger-indy/scripts/indy-ledger-script/indy-env-txn.sh
+++ platforms/hyperledger-indy/scripts/indy-ledger-script/indy-env-txn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 xterm -title "Docker Pool Setup" -hold -e "./pool-setup.sh" &
+xterm -title "Vault Setup" -hold -e "./vault-setup.sh" &
 xterm -title "Indy Pool Transactions" -hold -e "./indy-ledger.sh vault_addr vault_token admin_path admin_name identity_path identity_name identity_role pool_genesis_path"
+
+#Replace the above arguments with actual values
