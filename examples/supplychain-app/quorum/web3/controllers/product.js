24dafd8d1c728a90f5ca65ad5ca5ea597f9b62c2
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2020-02-10 12:12:08 -0500

    API product.js web3
    Signed-off-by: Hasan, Sadia A <sadia.a.hasan@accenture.com>

---
+++ examples/supplychain-app/quorum/web3/controllers/product.js
@@ -0,0 +1,149 @@
+var Web3 = require("web3");
+
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var address = "0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7";
+var abi = `[
+    {
+    "anonymous": false,
+    "inputs": [
+    {
+    "indexed": false,
+    "internalType": "string",
+    "name": "ID",
+    "type": "string"
+    }
+    ],
+    "name": "productAdded",
+    "type": "event"
+    },
+    {
+    "constant": true,
+    "inputs": [
+    {
+    "internalType": "uint256",
+    "name": "",
+    "type": "uint256"
+    }
+    ],
+    "name": "supplyChain",
+    "outputs": [
+    {
+    "internalType": "string",
+    "name": "productName",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "health",
+    "type": "string"
+    },
+    {
+    "internalType": "bool",
+    "name": "sold",
+    "type": "bool"
+    },
+    {
+    "internalType": "bool",
+    "name": "recalled",
+    "type": "bool"
+    },
+    {
+    "internalType": "string",
+    "name": "custodian",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "lastScannedAt",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "trackingID",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "containerID",
+    "type": "string"
+    }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+    },
+    {
+    "constant": false,
+    "inputs": [
+    {
+    "internalType": "string",
+    "name": "_productName",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "_health",
+    "type": "string"
+    },
+    {
+    "internalType": "bool",
+    "name": "_sold",
+    "type": "bool"
+    },
+    {
+    "internalType": "bool",
+    "name": "_recalled",
+    "type": "bool"
+    },
+    {
+    "internalType": "string",
+    "name": "_custodian",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "_lastScannedAt",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "_trackingID",
+    "type": "string"
+    },
+    {
+    "internalType": "string",
+    "name": "_containerID",
+    "type": "string"
+    }
+    ],
+    "name": "addProduct",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+    }
+    ]`;
+
+var productContract = new web3js.eth.Contract(abi, address);
+
+//POST METHODS
+
+let newProduct = {
+  productName: "Dextrose",
+  misc: { name: "Expensive Dextrose" },
+  trackingID: "0d15d7b8-caaa-468d-8b83-aae049b40f46",
+  counterparties: ["PartyB", "PartyC", "PartyD"]
+};
+
+productContract.methods
+  .createProduct(newProduct)
+  .send()
+  .then(response => {
+    //res.send(response);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+//GET METHODS
