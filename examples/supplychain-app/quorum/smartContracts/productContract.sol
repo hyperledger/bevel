0c9b3cef4dcd0892b35e7cd0d73abac7149f8c63
rayas-ng <rayas.ali@accenture.com>
2020-02-11 11:16:53 -0500

    completed newProductContract that creates a new product
    Signed-off-by: rayas-ng <rayas.ali@accenture.com>
    Signed-off-by: Hasan, Sadia A <sadia.a.hasan@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/productContract.sol
+++ examples/supplychain-app/quorum/smartContracts/productContract.sol
@@ -1,41 +1,76 @@
 pragma solidity 0.6.1;
 
-contract productContract {
-    string public productName;
-    string public health;
-    bool sold;
-    bool recalled;
-    //misc
-    string public custodian;
-    string public lastScannedAt;
-    string public trackingID;
-    int64 timestamp;
-    string containerID;
-    string[] participants;
+contract newProductContract {
+    
+    uint256 public count = 0;
+    
+   //  Product[] public supplyChain; //declaring an array of Products called supplyChain
+   
+    mapping(uint => Product) public supplyChain;
+    mapping(uint => Transaction) public transactionHistory;
+    mapping(uint => string) public miscellaneous;
+    
+    mapping(string => string) public counterparties;
+    
+    
+   // string[] public counterparties; //history of previous owners
 
 
-    function setProductName(string memory name) public { //create a product
-        productName = name;
-    }
-    function setHealth(string memory status) public {
-        health = status;
-    }
-    function setTrackingID(string memory ID) public {
-        trackingID = ID;
-    }
-    function setLastScannedAt(string memory location) public {
-        lastScannedAt = location;
+    address manufacturer;
+
+    modifier onlyManufacturer() {
+        require(msg.sender == manufacturer);
+        _;
     }
     
-   // function setCustodian(string memory owner) public {
-   //     custodian = owner;
-  //  }
 
-  //  function createProduct(custodian, productName) public {
+    struct Product{
+        string productName;
+        string health;
+        bool sold;
+        bool recalled;
+        string custodian; //who currently owns the product
+        string lastScannedAt;
+        string trackingID;
+        
+        //misc: Map<String,Any>;
+        
+    }
+    
 
-  //      if (custodian == "manufacturer") {
-  //          setProductName(productName);
- //       }
-  //  }
+    struct Transaction{
+            uint256 timestamp;
+            string containerID;
+    }
+    
+    
+    event productAdded (string ID);
+   
+    
+    
+     constructor() public{
+        manufacturer = msg.sender;
+    }
+    
+    
+    function addProduct(string memory _productName, string memory _health, bool _sold, 
+                        bool _recalled, string memory _custodian, string memory _lastScannedAt, 
+                        string memory _trackingID, string memory _containerID, string memory message) public onlyManufacturer {
+      uint256 _timestamp = block.timestamp;
+      
+      //  supplyChain.push(Product(_productName,_health,_sold,_recalled,_custodian,_lastScannedAt,_trackingID,_containerID));
+        count += 1;
+        supplyChain[count] = (Product(_productName,_health,_sold,_recalled,_custodian,_lastScannedAt,_trackingID));
+        emit productAdded(_trackingID);
+        
+        transactionHistory[count] = (Transaction(_timestamp, _containerID));
+        
+        counterparties[_trackingID] = _custodian;
+        miscellaneous[count] = message;
+        
 
+    }
+    
+    
+    
 }
