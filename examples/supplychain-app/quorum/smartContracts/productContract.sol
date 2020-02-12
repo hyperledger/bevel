349c8747d2dc5344c1829d932d84cfdb2e2857ef
rayas-ng <rayas.ali@accenture.com>
2020-02-12 16:53:29 -0500

    added a method for updating counter Parties
    Signed-off-by: rayas-ng <rayas.ali@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/productContract.sol
+++ examples/supplychain-app/quorum/smartContracts/productContract.sol
@@ -1,17 +1,16 @@
 pragma solidity 0.6.1; 
 
 contract productContract {
-
-    // supplyChain is a mapping of all the products created. The key begins at 1.
-    mapping(uint => Product) public supplyChain; 
+    
+    
+    mapping(uint => Product) public supplyChain; // supplyChain is a mapping of all the products created. The key begins at 1.
     mapping(string => Transaction) public transactionHistory;
     mapping(string => string) public miscellaneous;
-    // counterparties stores the current custodian plus the previous participants
-    mapping(string => string[]) public counterparties;
+    mapping(string => string[]) public counterparties; // counterparties stores the current custodian plus the previous participants
 
     address manufacturer; // stores the account address of the where this contract is deployed on in a variable called manufacturer.
-
-    modifier onlyManufacturer() { // only manufacturer can call the addProduct function. 
+    
+    modifier onlyManufacturer() {  // only manufacturer can call the addProduct function. 
         require(msg.sender == manufacturer);
         _;
     }
@@ -31,7 +30,7 @@ contract productContract {
     struct Transaction{
         uint256 timestamp;
         string containerID;
-
+            
     }
 
     event productAdded (string ID);
@@ -39,19 +38,19 @@ contract productContract {
     constructor() public{
         manufacturer = msg.sender;
     }
-
-    // The addProduct will create a new product only if they are the manufacturer.  Sold and Recall values are set to false by default. 
+    
+    // The addProduct will create a new product only if they are the manufacturer.  Sold and Recall values are set to false and containerID is "" when a product is newly created.
     function addProduct(string memory _productName, string memory _health, string memory _misc, string memory _trackingID, string memory _lastScannedAt) public onlyManufacturer {
-
+        
         uint256 _timestamp = block.timestamp;
         count += 1;
         bool _sold = false; 
         bool _recalled = false;
         string memory _containerID = "";
         string memory _custodian = "manufacturer";
-       // counterparties [_trackingID] = _custodian;
+       
         
-        transactionHistory[_trackingID] = (Transaction(_timestamp, _containerID));
+        transactionHistory[_trackingID] = (Transaction(_timestamp, _containerID)); // uses trackingID to get the timestamp and containerID.
         
         supplyChain[count] = (Product(_productName,_health,_sold,_recalled,_custodian,_trackingID,_lastScannedAt));
         
@@ -61,7 +60,7 @@ contract productContract {
         
         emit productAdded(_trackingID);
     }
-
+    
     //addCounterParties is a private method that updates the custodian of the product using the trackingID
     function addCounterParties(string memory _trackingID, string memory _custodian) internal{
         counterparties[_trackingID].push(_custodian);
