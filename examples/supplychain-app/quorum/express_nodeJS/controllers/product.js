41ec61b351fc2396ef03cb87ae3012831dc67e10
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2020-02-13 11:24:24 -0500

    implement env variables
    Signed-off-by: Hasan, Sadia A <sadia.a.hasan@accenture.com>

--- examples/supplychain-app/quorum/express_nodeJS/controllers/product.js
+++ examples/supplychain-app/quorum/express_nodeJS/controllers/product.js
@@ -5,12 +5,16 @@ var Web3 = require("web3");
 var express = require("express"),
   router = express();
 var bodyParser = require("body-parser");
+require('dotenv').config(".env");
+
+const web3Host = process.env['WEB3_LOCAL_HOST'];
+const port = process.env['PORT'];
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 //set up the express router
 router.use(bodyParser.json()); // for parsing application/json
-const port = 8000;
+// const port = 8000;
 router.get("/", (req, res) => res.send("You have reached the correct endpoint, please use complete API paths for requests"));
 router.listen(port, () =>
   console.log(`App listening on port ${port}!`)
@@ -18,131 +22,258 @@ router.listen(port, () =>
 
 /* address of smart contract
 */ 
-var address = "0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7";
+var address = process.env['TOADDRESS'];;
+var fromAddress = process.env
 
 /* ABI generated from smart contract
 * has definition for all methods and variables in contract
 */
 var abi = [
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "string",
-        name: "ID",
-        type: "string"
-      }
-    ],
-    name: "productAdded",
-    type: "event"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
-      }
-    ],
-    name: "supplyChain",
-    outputs: [
-      {
-        internalType: "string",
-        name: "productName",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "health",
-        type: "string"
-      },
-      {
-        internalType: "bool",
-        name: "sold",
-        type: "bool"
-      },
-      {
-        internalType: "bool",
-        name: "recalled",
-        type: "bool"
-      },
-      {
-        internalType: "string",
-        name: "custodian",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "lastScannedAt",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "trackingID",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "containerID",
-        type: "string"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "string",
-        name: "_productName",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "_health",
-        type: "string"
-      },
-      {
-        internalType: "bool",
-        name: "_sold",
-        type: "bool"
-      },
-      {
-        internalType: "bool",
-        name: "_recalled",
-        type: "bool"
-      },
-      {
-        internalType: "string",
-        name: "_custodian",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "_lastScannedAt",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "_trackingID",
-        type: "string"
-      },
-      {
-        internalType: "string",
-        name: "_containerID",
-        type: "string"
-      }
-    ],
-    name: "addProduct",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  }
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "ID",
+				"type": "string"
+			}
+		],
+		"name": "productAdded",
+		"type": "event"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_productName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_health",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_misc",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_trackingID",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_lastScannedAt",
+				"type": "string"
+			}
+		],
+		"name": "addProduct",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "count",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "counterparties",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getAllProducts",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "productName",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "health",
+						"type": "string"
+					},
+					{
+						"internalType": "bool",
+						"name": "sold",
+						"type": "bool"
+					},
+					{
+						"internalType": "bool",
+						"name": "recalled",
+						"type": "bool"
+					},
+					{
+						"internalType": "string",
+						"name": "custodian",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "trackingID",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "lastScannedAt",
+						"type": "string"
+					}
+				],
+				"internalType": "struct productContract.Product[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "miscellaneous",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "supplyChain",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "productName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "health",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "sold",
+				"type": "bool"
+			},
+			{
+				"internalType": "bool",
+				"name": "recalled",
+				"type": "bool"
+			},
+			{
+				"internalType": "string",
+				"name": "custodian",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "trackingID",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "lastScannedAt",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "transactionHistory",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "containerID",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 
 //instantiate the product smartcontract 
@@ -163,14 +294,11 @@ router.post("/api/v1/product", function(req, res) {
     .addProduct(
       newProduct.productName,
       "health",
-      true,
-      true,
-      "",
+      JSON.stringify(newProduct.misc),
       newProduct.trackingID,
-      "",
-      "" 
+      ""
     )
-    .send({ from: "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0" })
+    .send({ from: process.env['FROMADDRESS'] })
     .on("receipt", function(receipt) {
       // receipt example
       console.log(receipt);
