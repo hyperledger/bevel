4d936aaf09fd2ba82ec5f76b4ea044581890a6d2
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2020-02-13 09:56:32 -0500

    Revert "change name of folder"
    This reverts commit 08ac15525eae4982966ae36f27712255788bf989.

--- examples/supplychain-app/quorum/web3/controllers/index.js
+++ examples/supplychain-app/quorum/web3/controllers/index.js
@@ -1,12 +1,9 @@
-/* this file imports all express.js routes from container and product 
-* so all endpoints can run with one command
-*/
-
 var express = require('express')
   , router = express.Router()
 
 BASEURL = '/api/v1'
 
+// router.use( BASEURL + '/container', require('./container'))
 router.use( BASEURL + '/product', require('./product'))
 
 module.exports = router
