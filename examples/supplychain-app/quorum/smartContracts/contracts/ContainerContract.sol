ba3c211fbaed12fac641e9cc0917f4626ea91ac1
Lee <jane.b.lee@accenture.com>
2020-02-18 15:47:54 -0500

    merge
    Signed-off-by: Lee <jane.b.lee@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/contracts/ContainerContract.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/ContainerContract.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "./ProductContract.sol";
 
-contract containerContract is ProductContract{
+contract ContainerContract is ProductContract{
 
     address containerManufacturer; // stores the account address of the where this contract is deployed on in a variable called manufacturer.
 
@@ -12,7 +12,7 @@ contract containerContract is ProductContract{
     struct Container{
         string health;
         string misc;
-        string custodian; //who currently owns the product
+        address custodian; //who currently owns the product
         string lastScannedAt;
         string trackingID;
         uint timestamp;
@@ -20,7 +20,7 @@ contract containerContract is ProductContract{
         string[] participants;
     }
 
-    Container[] public containerSupplyChain;
+    Container[] public containers;
     mapping(string => Container) supplyChainMap;
 
     event containerAdded (string ID);
@@ -28,7 +28,7 @@ contract containerContract is ProductContract{
     event sendObject(Container container);
 
     constructor() public{
-        productManufacturer = msg.sender;
+        containerManufacturer = msg.sender;
     }
 
     function _addressToString(address x) private returns (string memory){
@@ -44,26 +44,43 @@ contract containerContract is ProductContract{
         string memory _lastScannedAt, string[] memory _counterparties) public returns (string memory) {
 
         uint256 _timestamp = block.timestamp;
-        string memory _custodian = _addressToString(msg.sender);
+        address _custodian = msg.sender;
         string memory _containerID = "";
 
-        containerSupplyChain.push(Container(_health, _misc, _custodian, _lastScannedAt, _trackingID, _timestamp, _containerID, _counterparties));
+        containers.push(Container(_health, _misc, _custodian, _lastScannedAt, _trackingID, _timestamp, _containerID, _counterparties));
         supplyChainMap[_trackingID] = Container(_health, _misc, _custodian, _lastScannedAt,
             _trackingID, _timestamp, _containerID, _counterparties);
         count++;
 
         emit containerAdded(_trackingID);
-        emit sendObject(containerSupplyChain[containerSupplyChain.length-1]);
+        emit sendObject(containers[containers.length-1]);
     }
 
     // the getAllContainers() function will return all containers in the containerSupplyChain[] array
     function getAllContainers() public returns(Container[] memory) {
-        emit sendArray(containerSupplyChain);
-        return containerSupplyChain;
+        emit sendArray(containers);
+        return containers;
     }
 
     function getSingleContainer(string memory _trackingID) public returns(Container memory) {
         emit sendObject(supplyChainMap[_trackingID]);
+        return supplyChainMap[_trackingID];
+    }
+
+    function packageTrackable(string memory _trackableTrackingID, string memory _containerTrackingID) public returns(string memory) {
+        // container exists
+        // trackable exists
+        // custodian of container is our identity
+        // custodian of trackable is our identity
+        // container's containerID is empty ""
+        // trackable's containerID is empty ""
+        // packaged trackable custodian is updated
+        if(bytes(supplyChainMap[_trackableTrackingID].trackingID).length > 0 && bytes(supplyChainMap[_containerTrackingID].trackingID).length > 0) {
+            if(supplyChainMap[_trackableTrackingID].custodian == msg.sender && supplyChainMap[_containerTrackingID].custodian == msg.sender) {
+
+            }
+        }
+
     }
 
 }
