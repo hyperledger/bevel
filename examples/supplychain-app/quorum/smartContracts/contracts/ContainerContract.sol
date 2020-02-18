a88e22f8b93b49f228ca256738f206d4a1fa37bd
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2020-02-18 15:48:36 -0500

    pure function
    Signed-off-by: Hasan, Sadia A <sadia.a.hasan@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/contracts/ContainerContract.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/ContainerContract.sol
@@ -31,7 +31,7 @@ contract containerContract is ProductContract{
         productManufacturer = msg.sender;
     }
 
-    function _addressToString(address x) private returns (string memory){
+    function _addressToString(address x) private pure returns (string memory){
     bytes memory b = new bytes(20);
     for (uint i = 0; i < 20; i++)
         b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
