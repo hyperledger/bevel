8d30f97b81b528584c93e9e2bb9562ea4dc1aef6
Lee <jane.b.lee@accenture.com>
2020-02-18 15:31:03 -0500

    Merge branch 'refactor-code' of https://github.com/sadia-hasan/blockchain-automation-framework into quorum-package-trackables merge Signed-off-by: Lee <jane.b.lee@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/contracts/ProductContract.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/ProductContract.sol
@@ -17,49 +17,30 @@ contract ProductContract is Permission {
         string[] participants;
     }
 
-<<<<<<< HEAD
- 
-
-    // FIXME: This should be the owner, there should be a way to have the manufacturer added and an array of manufacturers
-    constructor() public{
-        productManufacturer = msg.sender;
-    }
-
- 
-
-    // FIXME: move into a new contract called permissions
-    // only manufacturer Modifier checks that only the manufacturer can perform the task
-    modifier onlyManufacturer() {
-        require(msg.sender == productManufacturer, "This function can only be executed by the manufacturer");
-        _;
-    }
- 
-
-    // FIXME: This should be the owner, there should be a way to have the manufacturer added and an array of manufacturers
-
-    // The addProduct will create a new product only if they are the manufacturer.  Sold and Recall values are set to false and containerID is "" when a product is newly created.
-=======
     /**
     * @dev mapping of all the products created, the key begins at 1
     */
-    Product[] public products;
+    
+    Product[] public allProducts;
+    string[] public productKeys;
 
     /**
     * @dev counterparties stores the current custodian plus the previous participants
     */
-    mapping(string => address[]) public counterparties;
-    mapping(string => string) public miscellaneous;
+    mapping(string => Product) productSupplyChain;
     mapping (string => uint) trackingIDtoProductID;
+    mapping(string => string) public miscellaneous;
+    mapping(string => address[]) public counterparties;
 
     event productAdded (string ID);
-    event sendProductArray (Product[] array);
+    event sendArray (Product[] array);
     event sendProduct(Product product);
+
 
     /**
     * @return a new product
     * @dev Only if the caller is the manufacturer. Sold and Recall values are set to false and containerID is "" when a product is newly created.
     */
->>>>>>> 220a45e1f56c2a7d0d4510cf1e68043fb93afb6a
     function addProduct(string memory _productName,
         string memory _health,
         //FIXME: Update to an array of key --> value pairs
@@ -69,8 +50,6 @@ contract ProductContract is Permission {
         //FIXME: Add counterparties
         ) public returns (Product memory) {
 
- 
-
         uint256 _timestamp = now;
         bool _sold = false;
         bool _recalled = false;
@@ -78,8 +57,6 @@ contract ProductContract is Permission {
         address custodian = msg.sender;
         string[] memory participants;
         // participants[1] = "Test";
-
- 
 
          // uses trackingID to get the timestamp and containerID.
         Product memory newProduct = Product(_trackingID,
@@ -103,27 +80,17 @@ contract ProductContract is Permission {
         emit sendProduct(newProduct);
     }
 
-<<<<<<< HEAD
- 
 
-    //addCounterParties is a private method that updates the custodian of the product using the trackingID
-=======
     /**
     * @dev updates the custodian of the product using the trackingID
     */
->>>>>>> 220a45e1f56c2a7d0d4510cf1e68043fb93afb6a
     function addCounterParties(string memory _trackingID, address _custodian) internal{
         counterparties[_trackingID].push(_custodian);
     }
 
-<<<<<<< HEAD
- 
-
-=======
     /**
     * @return all products
     */
->>>>>>> 220a45e1f56c2a7d0d4510cf1e68043fb93afb6a
     function getAllProducts() public returns(Product[] memory) {
         for(uint i = 0; i < productKeys.length; i++){
             string memory trackingID = productKeys[i];
@@ -131,8 +98,6 @@ contract ProductContract is Permission {
         }
         emit sendArray(allProducts);
     }
-<<<<<<< HEAD
-=======
 
     //TODO what is this? Remove if unused
     // function packageTrackable(string memory _trackingID, string memory _containerID) public returns(...) {
@@ -148,5 +113,4 @@ contract ProductContract is Permission {
     * @return all containerless products
     */
     //TODO implement get containerless
->>>>>>> 220a45e1f56c2a7d0d4510cf1e68043fb93afb6a
 }
