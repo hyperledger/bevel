c265b762f333be42308054de76f50a3bc7177a20
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2020-02-20 12:13:26 -0500

    4 tests for container contract
    Signed-off-by: Hasan, Sadia A <sadia.a.hasan@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/test/ContainerContract.js
+++ examples/supplychain-app/quorum/smartContracts/test/ContainerContract.js
@@ -51,28 +51,6 @@ contract("ContainerContract", (accounts) => {
             containers[0].counterparties);
         const result = await contractInstance.getSingleContainer(containers[0].trackingID);
         assert.equal(result.receipt.status, true);
-        console.log(createResult);
-
+        assert.equal(createResult.logs[0].args.ID, result.receipt.logs[0].args[0].trackingID );
     })
-
-
 })
-
-
-
-// const CryptoZombies = artifacts.require("CryptoZombies");
-// const zombieNames = ["Zombie 1", "Zombie 2"];
-// contract("CryptoZombies", (accounts) => {
-//     let [alice, bob] = accounts;
-
-//     // start here
-
-//     it("should be able to create a new zombie", async () => {
-//         const contractInstance = await CryptoZombies.new();
-//         const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-//         assert.equal(result.receipt.status, true);
-//         assert.equal(result.logs[0].args.name,zombieNames[0]);
-//     })
-
-//     //define the new it() function
-// })
