b4c5258e5f212a34b3c04e90975f2e4fcc190cf0
Beth Shankle <elizabeth.shankle@accenture.com>
2020-02-20 07:28:12 -0500

    revert to old file names

--- examples/supplychain-app/quorum/smartContracts/contracts/containercontract.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/containercontract.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.6.1;
 pragma experimental ABIEncoderV2;
 
-import "./productcontract.sol";
+import "./ProductContract.sol";
 
 contract ContainerContract is ProductContract {
 
--- examples/supplychain-app/quorum/smartContracts/contracts/general.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/general.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.6.1;
 pragma experimental ABIEncoderV2;
 
-import "./containercontract.sol";
+import "./ContainerContract.sol";
 
 contract General is ContainerContract {
     /**
