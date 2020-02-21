26b75747cd7657555b2107b91882aaa0a0a9a9d6
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2020-02-21 14:03:47 -0500

    update
    Signed-off-by: Hasan, Sadia A <sadia.a.hasan@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/contracts/ContainerContract.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/ContainerContract.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "./ProductContract.sol";
 
-contract ContainerContract is ProductContract{
+contract ContainerContract is ProductContract {
     /**
     * @dev stores the account address of the where this contract is deployed on in a variable called manufacturer
     */
@@ -12,70 +12,99 @@ contract ContainerContract is ProductContract{
 
     uint256 public count = 0;
 
-    struct Container{
+    struct Container {
         string health;
         string misc;
-        string custodian; //who currently owns the product
+        address custodian; //who currently owns the product
         string lastScannedAt;
         string trackingID;
-        uint timestamp;
+        uint256 timestamp;
         string containerID;
         string[] participants;
+        string[] containerContents;
     }
 
-    Container[] public containerSupplyChain;
-    mapping(string => Container) supplyChainMap;
+    string[] public containerKeys;
+    Container[] public allContainers;
 
-    event containerAdded (string ID);
-    event sendArray (Container[] array);
-    event sendObject(Container container);
+    mapping(string => Container) containerSupplyChain;
 
-    function _addressToString(address x) private pure returns (string memory){
-        bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return string(b);
-    }
+    event sendString(string);
+    event sendArray(Container[]);
+    event sendObject(Container);
 
     /**
     * @return a new container
     * @dev Only if the caller is the manufacturer. Sold and Recall values are set to false and containerID is "" when a product is newly created.
     */
-    function addContainer(string memory _health, string memory _misc, string memory _trackingID,
-        string memory _lastScannedAt, string[] memory _counterparties) public returns (string memory) {
-
+    function addContainer(
+        string memory _health,
+        string memory _misc,
+        string memory _trackingID,
+        string memory _lastScannedAt,
+        string[] memory _counterparties
+    ) public returns (string memory) {
         uint256 _timestamp = block.timestamp;
-        string memory _custodian = _addressToString(msg.sender);
+        address _custodian = msg.sender;
         string memory _containerID = "";
+        string[] memory _containerContents;
 
-        containerSupplyChain.push(Container(_health, _misc, _custodian, _lastScannedAt, _trackingID, _timestamp, _containerID, _counterparties));
-        supplyChainMap[_trackingID] = Container(_health, _misc, _custodian, _lastScannedAt,
-            _trackingID, _timestamp, _containerID, _counterparties);
-        count++;
+        containerKeys.push(_trackingID);
+        containerSupplyChain[_trackingID] = Container(
+            _health,
+            _misc,
+            _custodian,
+            _lastScannedAt,
+            _trackingID,
+            _timestamp,
+            _containerID,
+            _counterparties,
+            _containerContents
+        );
 
-        emit containerAdded(_trackingID);
-        emit sendObject(containerSupplyChain[containerSupplyChain.length-1]);
+        emit sendString(_trackingID);
+        emit sendObject(containerSupplyChain[_trackingID]);
     }
 
     /**
     * @return all containers in the containerSupplyChain[] array
     */
-    function getAllContainers() public returns(Container[] memory) {
-        emit sendArray(containerSupplyChain);
-        return containerSupplyChain;
+    function getAllContainers() public returns (Container[] memory) {
+        delete allContainers;
+        for (uint256 i = 0; i < containerKeys.length; i++) {
+            string memory trackingID = containerKeys[i];
+            allContainers.push(containerSupplyChain[trackingID]);
+        }
+        emit sendArray(allContainers);
     }
 
     /**
     * @return one container by trackingID
     */
-    function getSingleContainer(string memory _trackingID) public returns(Container memory) {
-        emit sendObject(supplyChainMap[_trackingID]);
+    function getSingleContainer(string memory _trackingID) public returns (Container memory) {
+        if(bytes(containerSupplyChain[_trackingID].trackingID).length > 0) emit sendObject(containerSupplyChain[_trackingID]);
+        else emit sendString("No container exists with that tracking ID");
+        return containerSupplyChain[_trackingID];
     }
 
     /**
-    * @return container with updated custodian
     */
-    //TODO implement update custodian
+    function updateContainerCustodian(string memory _containerID) public {
+        containerSupplyChain[_containerID].custodian = msg.sender;
+
+        for (uint256 i = 0; i < containerSupplyChain[_containerID].containerContents.length; i++) {
+            if (bytes(productSupplyChain[containerSupplyChain[_containerID].containerContents[i]].trackingID).length > 0) {
+                productSupplyChain[containerSupplyChain[_containerID]
+                    .containerContents[i]]
+                    .custodian = msg.sender;
+            } else if (bytes(containerSupplyChain[containerSupplyChain[_containerID].containerContents[i]].trackingID).length > 0) {
+                updateContainerCustodian(
+                    containerSupplyChain[_containerID].containerContents[i]
+                );
+            }
+        }
+        emit sendObject(containerSupplyChain[_containerID]);
+    }
 
     /**
     * @return an updated container list with the package added
@@ -86,4 +115,5 @@ contract ContainerContract is ProductContract{
     * @return an updated container list with the package removed
     */
     //TODO implement unpackage
+
 }
