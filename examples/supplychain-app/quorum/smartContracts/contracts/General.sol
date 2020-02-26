1f65ab3e1fc89a3f5ecb7ff1ed4842e0b4751908
Lee <jane.b.lee@accenture.com>
2020-02-26 14:57:02 -0500

    scan function for ownership status
    Signed-off-by: Lee <jane.b.lee@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/contracts/General.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/General.sol
@@ -4,17 +4,23 @@ pragma experimental ABIEncoderV2;
 import "./ContainerContract.sol";
 
 contract General is ContainerContract {
+
+    event sendString(string);
+
     /**
     * @return whether a tracking ID is owned and if so whether the scanning user is the owner
     */
-    function scan(string memory _trackingID) public view returns(string memory) {
+    function scan(string memory _trackingID) public returns(string memory) {
         if((containerSupplyChain[_trackingID].custodian == msg.sender) || productSupplyChain[_trackingID].custodian == msg.sender) {
+            emit sendString("status: owned");
             return("{'status': 'owned'}");
         }
         if((containerSupplyChain[_trackingID].custodian == address(0)) || productSupplyChain[_trackingID].custodian == address(0)) {
+            emit sendString("status: new");
             return("{'status': 'new'}");
         }
         else {
+            emit sendString("status: unowned");
             return("{'status': 'unowned'}");
         }
     }
