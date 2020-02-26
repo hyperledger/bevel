738cec7375197e9c111b4c31027fd334c065ff53
rayas-ng <rayas.ali@accenture.com>
2020-02-26 09:32:33 -0500

    fixed merge issues
    Signed-off-by: rayas-ng <rayas.ali@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/migrations/2_product_contract.js
+++ examples/supplychain-app/quorum/smartContracts/migrations/2_product_contract.js
@@ -1,11 +1,5 @@
 const ContainerContract = artifacts.require("ContainerContract");
-<<<<<<< HEAD
-
-module.exports = function(deployer) {
-  deployer.deploy(ContainerContract);
-=======
  
 module.exports = function(deployer) {
  deployer.deploy(ContainerContract);
->>>>>>> 2310c1be73e4aba42e089c19638bf253f4e26289
 };
