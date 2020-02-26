0364dfedb146ca124f73e314ce9442e68ad6d6a2
Lee <jane.b.lee@accenture.com>
2020-02-26 13:45:45 -0500

    scan solidity function
    Signed-off-by: Lee <jane.b.lee@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/contracts/General.sol
+++ examples/supplychain-app/quorum/smartContracts/contracts/General.sol
@@ -7,7 +7,17 @@ contract General is ContainerContract {
     /**
     * @return whether a tracking ID is owned and if so whether the scanning user is the owner
     */
-    //TODO implement scan
+    function scan(string memory _trackingID) public view returns(string memory) {
+        if((containerSupplyChain[_trackingID].custodian == msg.sender) || productSupplyChain[_trackingID].custodian == msg.sender) {
+            return("{'status': 'owned'}");
+        }
+        if((containerSupplyChain[_trackingID].custodian == address(0)) || productSupplyChain[_trackingID].custodian == address(0)) {
+            return("{'status': 'new'}");
+        }
+        else {
+            return("{'status': 'unowned'}");
+        }
+    }
 
     /**
     * @return a list of objects with the location, timestamp of custodian change, and new custodian
