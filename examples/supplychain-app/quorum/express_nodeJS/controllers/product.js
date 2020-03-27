9460e4f32f57885a8259b76b3504ea4f53872d72
sownak.roy <sownak.roy@accenture.com>
2020-03-27 10:30:50 +0000

    Fix tessera and constellation errors
    Signed-off-by: sownak.roy <sownak.roy@accenture.com>
    Signed-off-by: sownak <sownak.roy@accenture.com>

--- examples/supplychain-app/quorum/express_nodeJS/controllers/product.js
+++ examples/supplychain-app/quorum/express_nodeJS/controllers/product.js
@@ -15,14 +15,14 @@ router.get('/containerless', function (req, res) {
   var containerlessArray = [];
   productContract.methods
     .getProductsLength()
-    .call({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+    .call({ from: fromAddress, gas: 6721975, gasPrice: "0" })
     .then(async response => {
       productCount = response;
       console.log("LENGTH ", response);
       for (var i = 1; i <= productCount; i++) { 
         var toPush = await productContract.methods
           .getContainerlessAt(i - 1)
-          .call({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+          .call({ from: fromAddress, gas: 6721975, gasPrice: "0" })
           var product = {};
           product.productName = toPush.productName;
           product.health = toPush.health;
@@ -57,7 +57,7 @@ router.get('/:trackingID?', function (req, res) {
     console.log(trackingID, "***");
     productContract.methods
       .getSingleProduct(req.params.trackingID)
-      .call({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+      .call({ from: fromAddress, gas: 6721975, gasPrice: "0" })
       .then(response => {
         var newProduct = response;
         var product = {};
@@ -96,14 +96,14 @@ router.get('/:trackingID?', function (req, res) {
     var displayArray = [];
     productContract.methods 
       .getProductsLength()
-      .call({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+      .call({ from: fromAddress, gas: 6721975, gasPrice: "0" })
       .then(async response => {
         arrayLength = response; 
         console.log("LENGTH ", response);
         for (var i = 1; i <= arrayLength; i++) {
           var toPush = await productContract.methods
             .getProductAt(i)
-            .call({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+            .call({ from: fromAddress, gas: 6721975, gasPrice: "0" })
           var product = {};
           product.productName = toPush.productName;
           product.health = toPush.health;
@@ -164,7 +164,7 @@ router.post('/', upload.array(), function (req, res) {
       "",
       newProduct.counterparties
     )
-    .send({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+    .send({ from: fromAddress, gas: 6721975, gasPrice: "0" })
     .on("receipt", function (receipt) {
       // receipt example
       if (receipt.status === true) {
@@ -195,7 +195,7 @@ router.put('/:trackingID/custodian', function (req, res) {
   console.log(longLatCoordinates);
   productContract.methods
     .updateCustodian(trackingID, longLatCoordinates)
-    .send({ from: fromAddress, gas: 6721975, gasPrice: "30000000" })
+    .send({ from: fromAddress, gas: 6721975, gasPrice: "0" })
     .then(response => {
       res.send(response)
     })
