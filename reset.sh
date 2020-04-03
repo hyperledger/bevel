1b7386ebb8f51de78f90102716a290b8ce661cc4
sownak.roy <sownak.roy@accenture.com>
2020-04-03 12:51:20 +0000

    Add reset.sh
    Signed-off-by: sownak.roy <sownak.roy@accenture.com>
    Signed-off-by: sownak <sownak.roy@accenture.com>

---
+++ reset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Starting build process..."
+
+echo "Adding env variables..."
+export PATH=/root/bin:$PATH
+
+#Path to k8s config file
+KUBECONFIG=/home/blockchain-automation-framework/build/config
+
+
+echo "Running the playbook..."
+exec ansible-playbook -vv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
