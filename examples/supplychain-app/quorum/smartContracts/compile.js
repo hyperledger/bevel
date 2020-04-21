cdb087ed02a773859a2ff69269e1202a29a3ff3c
Lakshya Kumar <56861637+lakshyakumar@users.noreply.github.com>
2020-04-21 14:35:35 +0530

    576  deploying smart contract (#596)
    * js script for deployment of smart contract
    
    Signed-off-by: lakshyakumar <lakshya.kumar@accenture.com>
    
    * adding a todo statement
    
    Signed-off-by: lakshyakumar <lakshya.kumar@accenture.com>
    
    * adding parameterized smartcode path
    
    Signed-off-by: lakshyakumar <lakshya.kumar@accenture.com>
    
    * bullet points and output is added
    
    Signed-off-by: lakshyakumar <lakshya.kumar@accenture.com>

---
+++ examples/supplychain-app/quorum/smartContracts/compile.js
@@ -0,0 +1,56 @@
+const path = require('path'); 
+const fs = require('fs');
+const solc = require('solc');
+
+
+var createInput = function(dirname) {
+  return new Promise(function(resolve, reject) {
+    fs.readdir(dirname, function (err, files) {
+        let data = {}
+        if(err){
+          console.log(err);
+        }
+        files.forEach(file=>{
+          let fileName = file.split(".");
+          if (fileName[1] === 'sol'){
+            data[file] = { content: fs.readFileSync(path.resolve(dirname,file),'utf8') }
+          }
+        })
+        resolve({...data})
+    });
+      
+  });
+}
+
+async function GetByteCode(numberOfIterations, dirname, entrypoint, contractName){
+
+  let source = await createInput(dirname).then((data)=>{
+    return (data);
+  });
+
+  const input = {
+      language: 'Solidity',
+      sources: {...source},
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: numberOfIterations
+        },
+          outputSelection: {
+              '*': {
+                  '*': [ '*' ]
+              }
+          }
+      }
+    };
+
+  var output = JSON.parse(solc.compile(JSON.stringify(input))); // compiling the smart contract using the main entrypoint file
+  var smartContract = {};
+  smartContract.bytecode = output.contracts[`${entrypoint}`][contractName].evm.bytecode.object;
+  smartContract.abi = output.contracts[`${entrypoint}`][contractName].abi;
+  smartContract.gasEstimates = output.contracts[`${entrypoint}`][contractName].evm.gasEstimates;
+  smartContract.totalCost = output.contracts[`${entrypoint}`][contractName].evm.gasEstimates.creation.totalCost;
+  return smartContract;
+}
+
+module.exports = {GetByteCode};
