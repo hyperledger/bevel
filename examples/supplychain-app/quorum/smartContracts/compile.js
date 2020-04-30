24843a2f9b92cce6c22a512ad77cc7016b6daf3a
Lakshya Kumar <56861637+lakshyakumar@users.noreply.github.com>
2020-04-30 14:20:56 +0530

    645 adding privateFor support (#648)
    Signed-off-by: lakshyakumar <lakshya.kumar@accenture.com>

--- examples/supplychain-app/quorum/smartContracts/compile.js
+++ examples/supplychain-app/quorum/smartContracts/compile.js
@@ -1,19 +1,19 @@
-const path = require('path'); 
-const fs = require('fs');
-const solc = require('solc');
+const path = require('path'); // For resolving paths
+const fs = require('fs'); // For reading files
+const solc = require('solc'); // solidity compiler
 
-
+//The function creates the input object by reading the folder provided
 var createInput = function(dirname) {
   return new Promise(function(resolve, reject) {
-    fs.readdir(dirname, function (err, files) {
+    fs.readdir(dirname, function (err, files) { // Reading the specified folder from parameter for .sol files
         let data = {}
         if(err){
           console.log(err);
         }
-        files.forEach(file=>{
+        files.forEach(file=>{ // For each file in the folder
           let fileName = file.split(".");
-          if (fileName[1] === 'sol'){
-            data[file] = { content: fs.readFileSync(path.resolve(dirname,file),'utf8') }
+          if (fileName[1] === 'sol'){ // Reading the file only if the extention is .sol
+            data[file] = { content: fs.readFileSync(path.resolve(dirname,file),'utf8') } // adding the content of file to the data object in perticular format required by the compiler
           }
         })
         resolve({...data})
@@ -22,9 +22,10 @@ var createInput = function(dirname) {
   });
 }
 
+//Main function to convert the solidity code in directory dirname to the bytecode and abi
 async function GetByteCode(numberOfIterations, dirname, entrypoint, contractName){
 
-  let source = await createInput(dirname)
+  let source = await createInput(dirname) // running the createInput function for returning the sources object
   .then((data)=>{
     return (data);
   })
@@ -32,15 +33,15 @@ async function GetByteCode(numberOfIterations, dirname, entrypoint, contractName
     console.log("error while compiling", err)
   });
 
-  const input = {
-      language: 'Solidity',
-      sources: {...source},
+  const input = { //for compiler purpose
+      language: 'Solidity', 
+      sources: {...source}, // from the createInput() function
       settings: {
         optimizer: {
           enabled: true,
-          runs: numberOfIterations
+          runs: numberOfIterations // code is optimized to run for numberOfIterations iterations
         },
-          outputSelection: {
+          outputSelection: { // format of output
               '*': {
                   '*': [ '*' ]
               }
@@ -49,12 +50,12 @@ async function GetByteCode(numberOfIterations, dirname, entrypoint, contractName
     };
 
   var output = JSON.parse(solc.compile(JSON.stringify(input))); // compiling the smart contract using the main entrypoint file
-  var smartContract = {};
-  smartContract.bytecode = output.contracts[`${entrypoint}`][`${contractName}`].evm.bytecode.object;
-  smartContract.abi = output.contracts[`${entrypoint}`][`${contractName}`].abi;
-  smartContract.gasEstimates = output.contracts[`${entrypoint}`][`${contractName}`].evm.gasEstimates;
+  var smartContract = {}; // Creating an object to return only the required data to the caller
+  smartContract.bytecode = output.contracts[`${entrypoint}`][`${contractName}`].evm.bytecode.object; // adding bytecode to the variable
+  smartContract.abi = output.contracts[`${entrypoint}`][`${contractName}`].abi; // adding abi to the smartCOntract variable
+  smartContract.gasEstimates = output.contracts[`${entrypoint}`][`${contractName}`].evm.gasEstimates; // adding gas estimation to the smartContract variable
   smartContract.totalCost = output.contracts[`${entrypoint}`][`${contractName}`].evm.gasEstimates.creation.totalCost;
   return smartContract;
 }
 
-module.exports = {GetByteCode};
+module.exports = {GetByteCode}; //Exporting the GetByteCode() function
