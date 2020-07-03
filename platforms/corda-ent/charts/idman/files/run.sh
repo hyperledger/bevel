c8cfff309a85d737c9716bc87d7dfa1f19e498c1
deepakkumardbd <d.bd.kumar@accenture.com>
2020-07-03 10:04:24 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: deepakkumardbd <d.bd.kumar@accenture.com>

--- platforms/corda-ent/charts/idman/files/run.sh
+++ platforms/corda-ent/charts/idman/files/run.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
-{{ if eq .Values.bashDebug true }}
-set -x
-{{ end }}
 
-#
 # main run
-#
 if [ -f {{ .Values.jarPath }}/identitymanager.jar ]
 then
-{{ if eq .Values.bashDebug true }}
     sha256sum {{ .Values.jarPath }}/identitymanager.jar 
     cat etc/idman.conf
-{{ end }}
     echo
     echo "CENM: starting Identity Manager process ..."
     echo
@@ -27,16 +20,8 @@ if [ "${EXIT_CODE}" -ne "0" ]
 then
     HOW_LONG={{ .Values.sleepTimeAfterError }}
     echo
-    echo "Notary initial registration failed - exit code: ${EXIT_CODE} (error)"
-    echo
+    echo "exit code: ${EXIT_CODE} (error)"
     echo "Going to sleep for requested ${HOW_LONG} seconds to let you login and investigate."
-    echo
-else
-    HOW_LONG={{ .Values.sleepTime }}
-    echo
-    echo "Notary initial registration: no errors - sleeping for requested ${HOW_LONG} seconds before disappearing."
-    echo
 fi
-
 sleep ${HOW_LONG}
 echo
