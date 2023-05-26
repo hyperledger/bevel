5e130610c3495fee4afba75ab4e6da66b4959b37
saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>
2023-05-26 11:39:00 +0000

    feat(besu): improve error handling mechanism for vault api calls
    This pull request introduces improvements to the error handling mechanism and enhances troubleshooting capabilities in the bevel-vault.sh script. The changes made include:
     • Implemented error handling at the individual function level.
     • Enhanced error message logging for better visibility into failures.
     • Improved error checking and validation.
     • Updated paths in respect of the function calls to reflect the latest changes.
    
    fixes #2260
    
    Signed-off-by: saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>

--- platforms/shared/charts/vault-script/scripts/bevel-vault.sh
+++ platforms/shared/charts/vault-script/scripts/bevel-vault.sh
@@ -1,29 +1,13 @@
-##############################################################################################
-#  Copyright Accenture. All Rights Reserved.
-#
-#  SPDX-License-Identifier: Apache-2.0
-##############################################################################################
-#!/bin/bash
-
-##############################################################################################################################
-################################################## Utility functions starts ##################################################
-##############################################################################################################################
-
-# This function validates hashicorp vault responses 
+# This function validates HashiCorp Vault responses
 function validateVaultResponseHashicorp {
-    if echo ${2} | grep "errors" || [[ "${2}" = "" ]]; then
-        echo "ERROR: unable to retrieve ${1}: ${2}"
-        exit 1
-    fi
-    if  [[ "$3" = "LOOKUPSECRETRESPONSE" ]]
-    then
+    if [[ $3 == "LOOKUPSECRETRESPONSE" ]]; then
         http_code=$(curl -fsS -o /dev/null -w "%{http_code}" \
-        --header "X-Vault-Token: ${VAULT_TOKEN}" \
-        ${VAULT_ADDR}/v1/${1})
+            --header "X-Vault-Token: ${VAULT_TOKEN}" \
+            "${VAULT_ADDR}/v1/${1}")
         curl_response=$?
-        if test "$http_code" != "200" ; then
+        if [[ $http_code != "200" ]]; then
             echo "Http response code from Vault - $http_code and curl_response - $curl_response"
-            if test "$curl_response" != "0"; then
+            if [[ $curl_response != "0" ]]; then
                 echo "Error: curl command failed with error code - $curl_response"
                 exit 1
             fi
@@ -31,54 +15,92 @@ function validateVaultResponseHashicorp {
     fi
 }
 
-##############################################################################################################################
-################################################## Hashicorp vault functions Starts ##########################################
-##############################################################################################################################
-
+# Initialize Vault token
 function initHashicorpVaultToken {
+    # Retrieve the Kubernetes service account token
     KUBE_SA_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-    VAULT_TOKEN=$(curl -sS --request POST ${VAULT_ADDR}/v1/auth/${KUBERNETES_AUTH_PATH}/login -H "Content-Type: application/json" -d \
-                '{"role":"'"${VAULT_APP_ROLE}"'","jwt":"'"${KUBE_SA_TOKEN}"'"}' | jq -r 'if .errors then . else .auth.client_token end')
+    # Request a Vault token using the Kubernetes authentication method
+    RESPONSE=$(curl -sS --request POST "${VAULT_ADDR}/v1/auth/${KUBERNETES_AUTH_PATH}/login" -H "Content-Type: application/json" -d \
+        '{"role":"'${VAULT_APP_ROLE}'","jwt":"'${KUBE_SA_TOKEN}'"}')
+    # Print the Vault API response
+    echo "Vault token API call response: $RESPONSE"
+
+    # Extract error message (if any) from the response using jq
+    ERROR=$(echo "$RESPONSE" | jq -r '.errors[0]')
+    # Extract the Vault secret data from the response using jq
+    VAULT_TOKEN=$(echo "$RESPONSE" | jq -r '.auth.client_token')
+
+    # Check if the Vault token is empty, null, or contains errors
+    if [[ $VAULT_TOKEN == "" || $VAULT_TOKEN == "null" || $VAULT_TOKEN == *"errors"* ]]; then
+        echo "Error: Failed to obtain Vault token."
+        echo "Error Details: $ERROR"
+        exit 1
+    else
+        echo "Vault token successfully obtained."
+        validateVaultResponseHashicorp 'vault login token' "$VAULT_TOKEN"
+    fi
 }
 
-#Arg1: Vault token; Arg2: Secret Path
+# Read Vault secret
 function readHashicorpVaultSecret {
-    # Hashicorp v2 secret data path format
-    #secret_path=$VAULT_SECRET_PATH/data/$1
-    # Curl to the vault server
-    VAULT_SECRET=$(curl --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR}/v1/${1} | jq -r 'if .errors then "null" else .data end')
+    # Send a request to Vault API to read a secret
+    RESPONSE=$(curl --header "X-Vault-Token: ${VAULT_TOKEN}" "${VAULT_ADDR}/v1/${1}")
+    # Print the Vault API response
+    echo "Vault read API call response: $RESPONSE"
+
+    # Extract error message (if any) from the response using jq
+    ERROR=$(echo "$RESPONSE" | jq -r '.errors[0]')
+    # Extract the Vault secret data from the response using jq
+    VAULT_SECRET=$(echo "$RESPONSE" | jq -r '.data')
+
+    # Check if the Vault Secret is empty, null, or contains errors
+    if [[ $VAULT_SECRET == "" || $VAULT_SECRET == "null" || $VAULT_SECRET == *"errors"* ]]; then
+        echo "Error: Failed to read Vault secret."
+        echo "Error Details: $ERROR"
+        exit 1
+    fi
 }
 
+# Write a secret to the Vault
 function writeHashicorpVaultSecret {
-    #secret_path=$VAULT_SECRET_PATH/data/$1
+    # Send a request to Vault API to write a secret
     VAULT_RESPONSE=$(curl \
-                  -H "X-Vault-Token: ${VAULT_TOKEN}" \
-                  -H "Content-Type: application/json" \
-                  -X POST \
-                  -d @${2} \
-                  ${VAULT_ADDR}/v1/${1})
-}
+        -H "X-Vault-Token: ${VAULT_TOKEN}" \
+        -H "Content-Type: application/json" \
+        -X POST \
+        -d @"${2}" \
+        "${VAULT_ADDR}/v1/${1}")
 
-##############################################################################################################################
-################################################## Vault main handler function ###############################################
-##############################################################################################################################
+    # Print the Vault API response
+    echo "Vault write API call response: ${VAULT_RESPONSE}"
+
+    # Check if the Vault API response indicates a failure
+    if [[ $VAULT_RESPONSE == "" || $VAULT_RESPONSE == "null" || $VAULT_RESPONSE == *"errors"* ]]; then
+        echo "Error: Failed to write to Vault"
+        exit 1
+    fi
+}
 
 vaultBevelFunc() {
-    if [[ $VAULT_TYPE = "hashicorp" ]]; then
-        if [[ $1 = "init" ]] 
-        then
-            initHashicorpVaultToken
-            echo $VAULT_TOKEN 
-        fi
-        if [[ $1 = "readJson" ]] 
-        then
-            readHashicorpVaultSecret "$2"
-            echo $VAULT_SECRET
-        fi
-        if [[ $1 = "write" ]] 
-        then
-            writeHashicorpVaultSecret "$2" "$3"
-            echo $VAULT_RESPONSE
-        fi
+    if [[ $VAULT_TYPE == "hashicorp" ]]; then
+        case $1 in
+            "init")
+                # Initialize Vault token
+                initHashicorpVaultToken
+                ;;
+            "readJson")
+                # Read Vault secret
+                readHashicorpVaultSecret "$2"
+                ;;
+            "write")
+                # Write a secret to the Vault
+                writeHashicorpVaultSecret "$2" "$3"
+                ;;
+            *)
+                # Invalid option
+                echo "Invalid option"
+                exit 1
+                ;;
+        esac
     fi
 }
