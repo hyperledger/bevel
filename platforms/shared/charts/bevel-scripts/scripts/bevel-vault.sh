4e47e2ca66407a930ab783b73aac7c4a27d5132f
saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>
2023-12-19 13:27:30 +0000

    feat(shared): Update bevel-vault.sh script for sh shell compatibility
    Change:
    Modified bevel-vault.sh script for sh shell compatibility.
    
    fixes #quick-fix
    
    Signed-off-by: saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>

--- platforms/shared/charts/bevel-scripts/scripts/bevel-vault.sh
+++ platforms/shared/charts/bevel-scripts/scripts/bevel-vault.sh
@@ -4,9 +4,9 @@
 #  SPDX-License-Identifier: Apache-2.0
 ##############################################################################################
 
-# This function validates HashiCorp Vault responses
-function validateVaultResponseHashicorp {
-    if [[ $2 == "LOOKUPSECRETRESPONSE" ]]; then
+# Validate HashiCorp Vault responses
+validateVaultResponseHashicorp() {
+    if [ "$2" = "LOOKUPSECRETRESPONSE" ]; then
         http_code=$(curl -fsS -o /dev/null -w "%{http_code}" \
             --header "X-Vault-Token: ${VAULT_TOKEN}" \
             "${VAULT_ADDR}/v1/${1}")
@@ -15,7 +15,7 @@ function validateVaultResponseHashicorp {
         echo "HTTP response code from Vault: $http_code"
         echo "Curl response code from Vault: $curl_response"
 
-        if [[ $http_code == "200" && $curl_response == "0" ]]; then
+        if [ "$http_code" = "200" ] && [ "$curl_response" = "0" ]; then
             echo "Validation successful for: $3"
         else
             echo "Validation failed for: $3"
@@ -24,8 +24,8 @@ function validateVaultResponseHashicorp {
     fi
 }
 
-# Initialize Vault token
-function initHashicorpVaultToken {
+# Initialize HashiCorp Vault token
+initHashicorpVaultToken() {
     # Retrieve the Kubernetes service account token
     KUBE_SA_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
     # Request a Vault token using the Kubernetes authentication method
@@ -40,7 +40,7 @@ function initHashicorpVaultToken {
     VAULT_TOKEN=$(echo "$RESPONSE" | jq -r '.auth.client_token')
 
     # Check if the Vault token is empty, null, or contains errors
-    if [[ $VAULT_TOKEN == "" || $VAULT_TOKEN == "null" || $VAULT_TOKEN == *"errors"* ]]; then
+    if [ -z "$VAULT_TOKEN" ] || [ "$VAULT_TOKEN" = "null" ] || echo "$VAULT_TOKEN" | grep -q "errors"; then
         echo "Error: Failed to obtain Vault token."
         echo "Error Details: $ERROR"
         exit 1
@@ -49,8 +49,8 @@ function initHashicorpVaultToken {
     fi
 }
 
-# Read Vault secret
-function readHashicorpVaultSecret {
+# Read HashiCorp Vault secret
+readHashicorpVaultSecret() {
     # Send a request to Vault API to read a secret
     RESPONSE=$(curl --header "X-Vault-Token: ${VAULT_TOKEN}" "${VAULT_ADDR}/v1/${1}")
     # Print the Vault API response
@@ -60,18 +60,18 @@ function readHashicorpVaultSecret {
     ERROR=$(echo "$RESPONSE" | jq -r '.errors[0]')
     # Extract the Vault secret data from the response using jq
     VAULT_SECRET=$(echo "$RESPONSE" | jq -r '.data.data')
-    
+
     # Flag to indicate that secrets are present in the vault or not
     SECRETS_AVAILABLE="no"
 
     # Stop further execution of code if an error is found
-    if [[ "$ERROR" != "" && "$ERROR" != "null" ]]; then
+    if [ -n "$ERROR" ] && [ "$ERROR" != "null" ]; then
         echo "Error: Failed to read Vault secret."
         echo "Error Details: $ERROR"
         exit 1
     else
         # Check if the Vault API response indicates a failure
-        if [[ "$VAULT_SECRET" != "" && "$VAULT_SECRET" != "null" && "$VAULT_SECRET" != *"errors"* ]]; then
+        if [ -n "$VAULT_SECRET" ] && [ "$VAULT_SECRET" != "null" ] && ! echo "$VAULT_SECRET" | grep -q "errors"; then
             validateVaultResponseHashicorp "${1}" "LOOKUPSECRETRESPONSE" "read api call"
             echo "Successfully obtained Vault Secret from the path ${VAULT_ADDR}/v1/${1}"
             echo "Vault Secret: $VAULT_SECRET"
@@ -83,8 +83,8 @@ function readHashicorpVaultSecret {
     fi
 }
 
-# Write a secret to the Vault
-function writeHashicorpVaultSecret {
+# Write a secret to the HashiCorp Vault
+writeHashicorpVaultSecret() {
     # Send a request to Vault API to write a secret
     VAULT_RESPONSE=$(curl \
         -H "X-Vault-Token: ${VAULT_TOKEN}" \
@@ -98,7 +98,7 @@ function writeHashicorpVaultSecret {
 
     # Stop further execution of code if an error is found
     # Check if the Vault API response indicates a failure
-    if [[ "$VAULT_RESPONSE" == "" || "$VAULT_RESPONSE" == "null" || "$VAULT_RESPONSE" == *"errors"* ]]; then
+    if [ -z "$VAULT_RESPONSE" ] || [ "$VAULT_RESPONSE" = "null" ] || echo "$VAULT_RESPONSE" | grep -q "errors"; then
         echo "Error: Failed to write to Vault at path ${VAULT_ADDR}/v1/${1}"
         exit 1
     else
@@ -107,8 +107,9 @@ function writeHashicorpVaultSecret {
     fi
 }
 
+# Main function for HashiCorp Vault operations
 vaultBevelFunc() {
-    if [[ $VAULT_TYPE == "hashicorp" ]]; then
+    if [ "$VAULT_TYPE" = "hashicorp" ]; then
         case $1 in
             "init")
                 # Initialize Vault token
